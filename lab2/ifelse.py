5c7a86104b2a4e3b333c581b75f924bb469e3e29
tomirismurtazina <tomirismurtazina@gmail.com>
2026-01-29 09:43:54 +0500

    if, elif, else statements

---
+++ lab2/ifelse.py
@@ -0,0 +1,51 @@
+#1
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+#2
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+#3
+day = 3
+
+if day == 1:
+  print("Monday")
+elif day == 2:
+  print("Tuesday")
+elif day == 3:
+  print("Wednesday")
+elif day == 4:
+  print("Thursday")
+elif day == 5:
+  print("Friday")
+elif day == 6:
+  print("Saturday")
+elif day == 7:
+  print("Sunday")
+
+#4
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+#5
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+
+#6
+a = 2
+b = 330
+print("A") if a > b else print("B")
