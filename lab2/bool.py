a1bb94939f41744057da33826efbc7435e68db8e
tomirismurtazina <tomirismurtazina@gmail.com>
2026-01-29 09:41:40 +0500

    bool

---
+++ lab2/bool.py
@@ -0,0 +1,35 @@
+#1
+print(10 > 9)
+print(10 == 9)
+print(10 < 9) 
+
+#2
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a") 
+
+#3
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+#4
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({}) 
+
+#5
+def myFunction() :
+  return True
+
+print(myFunction()) 
