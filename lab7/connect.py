107b0ea36b742463552249f69b6f924cda14b154
tomirismurtazina <tomirismurtazina@gmail.com>
2026-04-01 18:22:45 +0500

    lab 7

---
+++ lab7/connect.py
@@ -0,0 +1,14 @@
+import psycopg2
+from config import load_config
+
+def connect(config):
+    try:
+        with psycopg2.connect(**config) as con:
+            print("Connected to PostgreSQL server.")
+            return con
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
+
+if __name__=="__main__":
+    config=load_config()
+    connect(config)
